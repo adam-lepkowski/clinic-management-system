c77b82162ac32dc540da705edcdc8bea88cf512d
adam <adam.lepkowski@hotmail.com>
2023-01-22 15:17:12 +0100

    Add MainView

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class MainView(View):
+    """
+    Display main page.
+    """
+
+    def get(self, request):
+        return render(request, "main/index.html")
