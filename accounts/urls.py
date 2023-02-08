0892e54831e028e0297ef08d5d51223fbb157ee7
adam <adam.lepkowski@hotmail.com>
2023-02-08 21:11:31 +0100

    Add app_name and logout view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 import django.contrib.auth.views as auth_views
 
+
+app_name = "accounts"
+
 urlpatterns = [
     path("login", auth_views.LoginView.as_view(
         template_name="accounts/login.html",
-        redirect_authenticated_user=True)
-    )
+        redirect_authenticated_user=True)),
+    path("logout", auth_views.LogoutView.as_view(), name="logout")
 ]
