65cffa03293fc08106e5470889a4fd895d1f31b2
adam <adam.lepkowski@hotmail.com>
2023-02-08 20:43:28 +0100

    Require login to main page

--- main/views.py
+++ main/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class MainView(View):
+class MainView(LoginRequiredMixin, View):
     """
     Display main page.
     """
-
+    
     def get(self, request):
         """
         Render main page.
