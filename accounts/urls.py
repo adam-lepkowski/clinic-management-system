e593357cd4323c4652762deeb15af60d04a40b92
adam <adam.lepkowski@hotmail.com>
2023-02-09 15:39:51 +0100

    Route profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 import django.contrib.auth.views as auth_views
 
+from .views import ProfileView
+
 
 app_name = "accounts"
 
@@ -8,5 +10,6 @@ urlpatterns = [
     path("login", auth_views.LoginView.as_view(
         template_name="accounts/login.html",
         redirect_authenticated_user=True)),
-    path("logout", auth_views.LogoutView.as_view(), name="logout")
+    path("logout", auth_views.LogoutView.as_view(), name="logout"),
+    path("", ProfileView.as_view(), name="profile")
 ]
