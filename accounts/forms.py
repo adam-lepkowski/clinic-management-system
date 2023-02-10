935fc98922982b551153a86b140199521c4c1524
adam <adam.lepkowski@hotmail.com>
2023-02-10 11:08:45 +0100

    Add PwdChangeForm
    Inherit from PasswordChangeForm to modify labels.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,12 @@
-from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import PasswordChangeForm
 
 
-class ProfileForm(ModelForm):
+class ProfileForm(forms.ModelForm):
+    """
+    Represent uneditable user profile data.
+    """
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,3 +18,19 @@ class ProfileForm(ModelForm):
     class Meta:
         model = User
         fields = ["username", "first_name", "last_name", "email"]
+
+
+class PwdChangeForm(PasswordChangeForm):
+    """
+    Change currently logged in user password.
+    """
+        
+    new_password2 = forms.CharField(
+        label=("Confirm new password"),
+        strip=False,
+        widget=forms.PasswordInput(attrs={"autocomplete": "new-password"}),
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ""
