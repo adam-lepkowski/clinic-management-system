86dfd2ac3d97fe45ad9f0a7479a3046dc70811f0
adam <adam.lepkowski@hotmail.com>
2023-02-10 11:17:26 +0100

    Route change password view

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 import django.contrib.auth.views as auth_views
 
 from .views import ProfileView
+from .forms import PwdChangeForm
 
 
 app_name = "accounts"
@@ -11,5 +12,8 @@ urlpatterns = [
         template_name="accounts/login.html",
         redirect_authenticated_user=True)),
     path("logout", auth_views.LogoutView.as_view(), name="logout"),
-    path("", ProfileView.as_view(), name="profile")
+    path("", ProfileView.as_view(), name="profile"),
+    path("change-password", auth_views.PasswordChangeView.as_view(
+            template_name="accounts/change_password.html",
+            form_class=PwdChangeForm))
 ]
