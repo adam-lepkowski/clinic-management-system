145fcc89140206ab1a42802c7cdf1736ab954d32
adam <adam.lepkowski@hotmail.com>
2023-02-11 19:19:30 +0100

    Add PatientForm and AddressForm
    Both inherit from ModelForm

---
+++ patients/forms.py
@@ -0,0 +1,37 @@
+from django.forms import ModelForm
+
+from .models import Patient, Address
+
+
+class PatientForm(ModelForm):
+    """
+    Represent Patient model fields.
+    """
+
+    class Meta:
+        model = Patient
+        fields = [
+            "first_name",
+            "last_name",
+            "date_of_birth",
+            "personal_id",
+            "email",
+            "phone"
+        ]
+
+
+class AddressForm(ModelForm):
+    """
+    Represent Address model fields.
+    """
+
+    class Meta:
+        model = Address
+        fields = [
+            "street",
+            "number",
+            "apartment",
+            "zip_code",
+            "city",
+            "country"
+        ]
