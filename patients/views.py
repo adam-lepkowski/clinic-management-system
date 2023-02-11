b345c04a9c6e635866bc3d43f1b2dc7824622254
adam <adam.lepkowski@hotmail.com>
2023-02-11 19:20:53 +0100

    Add RegistrationView
    Allow only get requests at the moment.

--- patients/views.py
+++ patients/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .forms import PatientForm, AddressForm
+
+
+class RegistrationView(View):
+    """
+    Display patient registration form.
+    """
+
+    def get(self, request):
+        """
+        Display empty patient registration form.
+        """
+
+        patient_form = PatientForm(prefix="patient")
+        address_form = AddressForm(prefix="address")
+        context = {
+            "patient_form": patient_form,
+            "address_form": address_form
+        }
+        return render(request, "patients/register.html", context)
