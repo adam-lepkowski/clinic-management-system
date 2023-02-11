b4db23f8f911032715ee753b003da08cdf8ce0be
adam <adam.lepkowski@hotmail.com>
2023-02-11 21:42:51 +0100

    SuccessRegistrationView path

--- patients/urls.py
+++ patients/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import RegistrationView
+from .views import RegistrationView, SuccessRegistrationView
 
 app_name = "patients"
 
 urlpatterns = [
-    path("register", RegistrationView.as_view(), name="register")
+    path("register", RegistrationView.as_view(), name="register"),
+    path("registered", SuccessRegistrationView.as_view(), name="registered")
 ]
