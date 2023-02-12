d7b344bf62c9176038b0b595fdcfc12f2a2f9a68
adam <adam.lepkowski@hotmail.com>
2023-02-12 12:26:59 +0100

    Test SuccessRegistrationView

--- patients/tests/test_views.py
+++ patients/tests/test_views.py
@@ -45,3 +45,29 @@ class TestRegistrationView(TestCase):
         response = self.client.post("/patient/register", data, follow=True)
         self.assertTemplateNotUsed(response, "/patient/registered")
         self.assertEqual(len(response.redirect_chain), 0)
+
+
+class TestSuccessRegistrationView(TestCase):
+
+    def setUp(self):
+        self.address = {
+            "address-street": "Test Lane",
+            "address-number": "12a",
+            "address-apartment": "123",
+            "address-zip_code": "00-000",
+            "address-city": "Testington",
+            "address-country": "Republic of Testland"
+        }
+        self.patient = {
+            "patient-first_name": "Johnny",
+            "patient-last_name": "Test",
+            "patient-date_of_birth": "2022-12-12",
+            "patient-personal_id": "12345678911",
+            "patient-email": "email@email.com",
+            "patient-phone": "0123456789"
+        }
+    
+    def test_redirect_if_no_form_submitted(self):
+        response = self.client.get("/patient/registered", follow=True)
+        self.assertFalse(self.client.session.get("patient-registered", False))
+        self.assertRedirects(response, "/patient/register")
