05da768d5fbdef9d5c9ace1fa56b3c395b3ed7d2
adam <adam.lepkowski@hotmail.com>
2023-02-12 14:58:59 +0100

    Add PatientView url

--- patients/urls.py
+++ patients/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import RegistrationView, SuccessRegistrationView
+from .views import RegistrationView, SuccessRegistrationView, PatientView
 
 app_name = "patients"
 
 urlpatterns = [
     path("register", RegistrationView.as_view(), name="register"),
-    path("registered", SuccessRegistrationView.as_view(), name="registered")
+    path("registered", SuccessRegistrationView.as_view(), name="registered"),
+    path("<int:pk>", PatientView.as_view(), name="patient")
 ]
