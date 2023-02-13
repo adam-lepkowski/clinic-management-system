7af1cbe61de2a7a21cee816d715ad02d6c5b4695
adam <adam.lepkowski@hotmail.com>
2023-02-13 21:24:49 +0100

    Search results url

--- patients/urls.py
+++ patients/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import RegistrationView, SuccessRegistrationView, PatientView
+from .views import (
+    RegistrationView, SuccessRegistrationView, PatientView, SearchResultsView)
 
 app_name = "patients"
 
 urlpatterns = [
     path("register", RegistrationView.as_view(), name="register"),
     path("registered", SuccessRegistrationView.as_view(), name="registered"),
-    path("<int:pk>", PatientView.as_view(), name="patient")
+    path("<int:pk>", PatientView.as_view(), name="patient"),
+    path("search-results", SearchResultsView.as_view(), name="search_results")
 ]
