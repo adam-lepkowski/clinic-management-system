186f6ca9373bb364815a8cc98ec9c5d9b9b0c4ad
adam <adam.lepkowski@hotmail.com>
2023-02-22 21:25:11 +0100

    Test Schedule user removal

--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from datetime import date
 
-# Create your tests here.
+from django.test import TestCase
+from django.contrib.auth.models import User
+
+from ..models import Schedule
+
+
+class TestSchedule(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create(
+            username="TestUser",
+            first_name="Teston",
+            last_name="Testingly"
+        )
+        self.schedule = Schedule.objects.create(
+            date="2023-01-01",
+            start="08:00",
+            end="16:00",
+            employee=self.user
+        )
+    
+    def test_remove_user_sets_schedule_null(self):
+        self.user.delete()
+        schedule = Schedule.objects.get(id=1)
+        self.assertEqual(schedule.employee, None)
+        self.assertEqual(schedule.date, date(2023, 1, 1))
