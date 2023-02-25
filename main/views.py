fc288469856b12a954b2a92557ab511febf436d8
adam <adam.lepkowski@hotmail.com>
2023-02-25 20:50:00 +0100

    Add ScheduleSearchView

--- main/views.py
+++ main/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from .forms import ScheduleSearchForm
+
 
 class MainView(LoginRequiredMixin, View):
     """
@@ -14,3 +16,21 @@ class MainView(LoginRequiredMixin, View):
         """
         
         return render(request, "main/index.html")
+
+
+class ScheduleSearchView(View):
+    """
+    Display schedule search form
+    """
+
+    def get(self, request):
+        """
+        Render empty schedule form.
+        """
+        
+        form = ScheduleSearchForm()
+        context = {
+            "form": form
+        }
+
+        return render(request, "main/schedule.html", context)
