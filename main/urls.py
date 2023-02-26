6b03f6afad7edd2c4df74e5ee52dd21b46007569
adam <adam.lepkowski@hotmail.com>
2023-02-26 18:26:26 +0100

    Path to search-results

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,9 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.MainView.as_view(), name="main"),
-    path("schedule/search", views.ScheduleSearchView.as_view(), name="schedule")
+    path("schedule/search", views.ScheduleSearchView.as_view(), name="schedule"),
+    path(
+        "schedule/search-results",
+        views.ScheduleListView.as_view(),
+        name="search_results")
 ]
