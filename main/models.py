2882d335574dc4e695a54dfc2679864b4d06f82e
adam <adam.lepkowski@hotmail.com>
2023-03-02 19:55:51 +0100

    Add emp_full_name and overwrite __str__

--- main/models.py
+++ main/models.py
@@ -6,8 +6,14 @@ class Schedule(models.Model):
     """
     Employee shift date, start time and end time.
     """
-    
+
     date = models.DateField()
     start = models.TimeField()
     end = models.TimeField()
     employee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.emp_full_name()} {self.date}"
+
+    def emp_full_name(self):
+        return f"{self.employee.first_name} {self.employee.last_name}"
