284584374597c5d3f6e258993f6826088aaaf0bc
adam <adam.lepkowski@hotmail.com>
2023-03-02 20:30:00 +0100

    Add get_day_schedule

--- main/utils.py
+++ main/utils.py
@@ -28,3 +28,30 @@ def get_appointment_times(schedule):
 
     return available_hours
 
+
+def get_day_schedule(schedules):
+    """
+    Get all available employees working hours at a given day.
+
+    Parameters
+    ----------
+    schedules : list
+                Contains Schedule objects.
+
+    Returns
+    ----------
+    list
+        dictionaries of appointment details.
+    """
+
+    day_schedule_by_available_hours = []
+    for schedule in schedules:
+        available_hours = get_appointment_times(schedule)
+        for hour in available_hours:
+            appointment_details = {
+                "employee_id": schedule.employee__id,
+                "employee_full_name": schedule.emp_full_name,
+                "date": schedule.date,
+                "hour": hour}
+            day_schedule_by_available_hours.append(appointment_details)
+    return day_schedule_by_available_hours
