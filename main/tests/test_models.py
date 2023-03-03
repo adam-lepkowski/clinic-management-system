6e46110b06a57f8c0a6aa6821590c7199da9526c
adam <adam.lepkowski@hotmail.com>
2023-03-03 17:51:22 +0100

    Test adding same work date twice

--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -2,6 +2,7 @@ from datetime import date
 
 from django.test import TestCase
 from django.contrib.auth.models import User
+from django.db.utils import IntegrityError
 
 from ..models import Schedule
 
@@ -20,9 +21,17 @@ class TestSchedule(TestCase):
             end="16:00",
             employee=self.user
         )
-    
+
     def test_remove_user_sets_schedule_null(self):
         self.user.delete()
         schedule = Schedule.objects.get(id=1)
         self.assertEqual(schedule.employee, None)
         self.assertEqual(schedule.date, date(2023, 1, 1))
+
+    def test_cant_schedule_the_same_day_twice_single_employee(self):
+        with self.assertRaises(IntegrityError):
+            Schedule.objects.create(
+                date="2023-01-01",
+                start="08:00",
+                end="16:00",
+                employee=self.user)
