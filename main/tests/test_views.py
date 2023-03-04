f18d5d60882c7f4f255f8721104d4bf3cd4e85fe
adam <adam.lepkowski@hotmail.com>
2023-03-04 17:38:43 +0100

    TestScheduleSearchView with login requirements

--- main/tests/test_views.py
+++ main/tests/test_views.py
@@ -1,20 +1,31 @@
 from unittest.mock import patch, call
 
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from ..forms import ScheduleSearchForm
 
 
 class TestScheduleSearchView(TestCase):
+    def setUp(self):
+        User.objects.create_user(
+            username="test_name",
+            email="test@email.com",
+            password="test_pw"
+        )
 
     def test_get(self):
+        self.client.login(username="test_name", password="test_pw")
         response = self.client.get("/schedule/search")
         self.assertIsInstance(response.context["form"], ScheduleSearchForm)
         self.assertTemplateUsed(response, "main/schedule.html")
 
     def test_ajax_get(self):
+        self.client.login(username="test_name", password="test_pw")
         response = self.client.get(
-            "/schedule/search", HTTP_X_REQUESTED_WITH="XMLHttpRequest")
+            "/schedule/search",
+            HTTP_X_REQUESTED_WITH="XMLHttpRequest"
+        )
         self.assertTemplateUsed(response, "main/includes/specialists.html")
 
 
