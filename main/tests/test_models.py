3704f3cfbe75215184dae9688fbb7013df09edfc
adam <adam.lepkowski@hotmail.com>
2023-03-05 17:23:35 +0100

    Test Appointment model

--- main/tests/test_models.py
+++ main/tests/test_models.py
@@ -1,10 +1,13 @@
 from datetime import date
 
-from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from django.core.exceptions import ValidationError
 from django.db.utils import IntegrityError
+from django.test import TestCase
+from django.utils import timezone
 
-from ..models import Schedule
+from patients.models import Patient
+from ..models import Schedule, Appointment
 
 
 class TestSchedule(TestCase):
@@ -34,4 +37,67 @@ class TestSchedule(TestCase):
                 date="2023-01-01",
                 start="08:00",
                 end="16:00",
-                employee=self.user)
+                employee=self.user
+            )
+
+
+class TestAppointment(TestCase):
+
+    def setUp(self):
+        self.physicians_group = Group.objects.create(
+            name="physicians"
+        )
+        self.patient = Patient.objects.create(
+            first_name="Johnny",
+            last_name="Test",
+            date_of_birth="2022-12-12",
+            personal_id="12345678911",
+            email="email@email.com",
+            phone="0123456789",
+            address=None
+        )
+
+    def test_appointment_with_not_a_doctor_not_possible(self):
+        not_doctor = User.objects.create_user(
+            username="TestUserNotADoctor",
+            first_name="NotDoctor",
+            last_name="NotDoctorly"
+        )
+        Schedule.objects.create(
+            date=timezone.now(),
+            start="08:00",
+            end="16:00",
+            employee=not_doctor
+        )
+        with self.assertRaises(ValidationError):
+            Appointment.objects.create(
+                datetime=timezone.now(),
+                patient=self.patient,
+                doctor=not_doctor,
+                purpose="Toothache",
+                examination=None,
+                diagnosis=None,
+                advice=None,
+                prescription=None,
+                took_place=False
+            )
+
+    def test_appointment_with__doctor_not_on_schedule_not_possible(self):
+        doctor = User.objects.create_user(
+            username="TestUserNotADoctor",
+            first_name="NotDoctor",
+            last_name="NotDoctorly"
+        )
+        doctor.groups.add(self.physicians_group)
+        with self.assertRaises(ValidationError):
+            Appointment.objects.create(
+                datetime=timezone.now(),
+                patient=self.patient,
+                doctor=doctor,
+                purpose="Toothache",
+                examination=None,
+                diagnosis=None,
+                advice=None,
+                prescription=None,
+                took_place=False
+            )
