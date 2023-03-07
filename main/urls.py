e38611b4397d5a348978e237ac130d28e927b0a6
adam <adam.lepkowski@hotmail.com>
2023-03-07 21:07:31 +0100

    AppointmentConfirmView path

--- main/urls.py
+++ main/urls.py
@@ -6,10 +6,24 @@ from . import views
 app_name = "main"
 
 urlpatterns = [
-    path("", views.MainView.as_view(), name="main"),
-    path("schedule/search", views.ScheduleSearchView.as_view(), name="schedule"),
+    path(
+        "",
+        views.MainView.as_view(),
+        name="main"
+    ),
+    path(
+        "schedule/search",
+        views.ScheduleSearchView.as_view(),
+        name="schedule"
+    ),
     path(
         "schedule/search-results",
         views.ScheduleListView.as_view(),
-        name="search_results")
+        name="search_results"
+    ),
+    path(
+        "appointment/confirm",
+        views.AppointmentConfirmView.as_view(),
+        name="confirm_appointment"
+    )
 ]
