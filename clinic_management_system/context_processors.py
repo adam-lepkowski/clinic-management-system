636cab4cd8281153726f5c05eb5a0ba94b9bbed6
adam <adam.lepkowski@hotmail.com>
2023-03-14 20:13:35 +0100

    Add get_user_group

--- clinic_management_system/context_processors.py
+++ clinic_management_system/context_processors.py
@@ -14,3 +14,23 @@ def get_kit(request):
     data = {}
     data["kit"] = os.environ["FAWESOME_KIT"]
     return data
+
+
+def user_group(request):
+    """
+    Check if logged belongs to nurses and physicians group.
+
+    Returns
+    ----------
+    dict
+        contains bool values for membership in nurses and physicians groups
+    """
+
+    user = request.user
+    is_nurse = user.groups.filter(name__iexact="nurses").exists()
+    is_doctor = user.groups.filter(name__iexact="physicians").exists()
+    data = {
+        "is_nurse": is_nurse,
+        "is_doctor": is_doctor
+    }
+    return data
